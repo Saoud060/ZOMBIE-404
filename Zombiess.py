7a535f101734d095cfad1838960dd33b558e9354
Saoud060 <63461235+Saoud060@users.noreply.github.com>
2023-10-26 18:32:45 +0500

    Update and rename Zombiess to Zombiess.py

---
+++ Zombiess.py
@@ -0,0 +1,13 @@
+import os
+import platform
+
+os.system('git pull')
+
+# Display a message indicating that the tool is working
+print("Wait, working on the tool...")
+
+adina = platform.architecture()[0]
+if adina == "32bit":
+    exit("Coming soon for 32-bit architecture")
+elif adina == "64bit":
+    __import__("BS")
